282982104c0c91cb5e141e2168d53a0fcf635668
go.montoya <go.montoya@duocuc.cl>
2023-05-01 22:30:04 -0400

    Carrusel a medias

--- js/script.js
+++ js/script.js
@@ -1,7 +1,56 @@
+// Navbar
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
+}
+
+// CArrusel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1}
+    if (n < 1 ) {slideIndex =  slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += "active";
+}
+
+// Slide automático
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides(){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i= 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if(slideIndex > slides.length){
+        slideIndex = 1
+    }
+    slides[slideIndex - 1 ].style.display = "block"
+    setTimeout(showSlides, 10000);
+    // Cambiar tiempo
 }
